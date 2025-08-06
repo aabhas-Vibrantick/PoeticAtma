dc8fbade613f0fb77e71de338134fcd9fa1537e2
Aabhas Jena <aabhasjena2003@gmail.com>
2025-08-06 17:21:40 +0530

    Google Tag Manager

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-
+import TagManager from 'react-gtm-module';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const tagManagerArgs = {
+  gtmId: 'GTM-WQR5LXJT'
+};
+
+TagManager.initialize(tagManagerArgs);
 root.render(
   <React.StrictMode>
     <App />
